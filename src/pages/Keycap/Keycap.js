060d2f1436c7933e822151f225a1c94fc220f27c
Hoang-261 <huyhoang.tv.hy@gmail.com>
2024-07-18 00:23:36 +0700

    update

--- src/pages/Keycap/Keycap.js
+++ src/pages/Keycap/Keycap.js
@@ -1,5 +1,9 @@
 function Keycap() {
-    return <div>Keycap</div>;
+    return (
+        <div className="m-5">
+            <h2>Update ...</h2>
+        </div>
+    );
 }
 
 export default Keycap;
--- src/pages/Mouse/Mouse.js
+++ src/pages/Mouse/Mouse.js
@@ -1,5 +1,9 @@
 function Mouse() {
-    return <div>Mouse</div>;
+    return (
+        <div className="m-5">
+            <h2>Update ...</h2>
+        </div>
+    );
 }
 
 export default Mouse;
